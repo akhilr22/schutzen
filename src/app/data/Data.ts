22b0d93438467efecbe16c08d4df06bcf02d6ec7
Akhil R <akhilr222@gmail.com>
2024-12-24 20:29:42 +0530

    Content added in homepage

--- src/app/data/Data.ts
+++ src/app/data/Data.ts
@@ -4,18 +4,18 @@ export const homePageData = {
         home_p_text1: "Schutzen transforms your facility's existing wireless network into a powerful, cost-effective, and easy-to-deploy FDA-cleared solution for vital signs monitoring",
     },
     whyChooseUs: {
-        img_url_1: "",
+        img_url_1: "innovative_technology.png",
         title_1: "Innovative Technology",
-        desc_1: "Our products leverage cutting-edge technology for accurate and reliable monitoring.",
+        desc_1: "Our products are designed to meet these needs by integrating the latest advancements in technology to deliver exceptional results. From real-time patient monitoring systems to AI-driven diagnostic tools, we ensure that healthcare providers have access to the most accurate and up-to-date information.",
         img_url_2: "",
         title_2: "Improved Patient Outcomes",
-        desc_2: "Enhanced monitoring and timely interventions lead to better patient care.",
+        desc_2: "In the healthcare sector, improved patient outcomes are the ultimate goal, and timely, data-driven interventions play a pivotal role in achieving this. With the integration of enhanced monitoring technologies, healthcare providers can track patient conditions in real-time, gaining critical insights that allow for informed decision.",
         img_url_3: "",
         title_3: "Streamlined Workflows",
-        desc_3: "Automated reporting and alerts reduce administrative burdens.",
+        desc_3: "In the healthcare sector, administrative tasks often take valuable time away from patient care. By streamlining workflows with automated reporting and alerts, our solutions significantly reduce the burden on healthcare professionals, allowing them to focus more on delivering high-quality care",
         img_url_4: "",
         title_4: "Global Accreditation",
-        desc_4: "Trust our products, backed by 21 USA patents and global accreditation.",
+        desc_4: "In the healthcare sector, the reliability and efficacy of medical products are critical to ensuring patient safety and quality care. Our products are built on a foundation of innovation and trust, backed by 21 USA patents and a range of global accreditations.",
     },
     monitoring: {
         // Add monitoring-related data here if needed
